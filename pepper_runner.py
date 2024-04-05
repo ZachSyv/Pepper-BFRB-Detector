073981dac87218b2aa365e9737480d76943e3a61
ZachSyv <zachary@syvenky.com>
2024-04-05 14:37:02 -0700

    Create pepper_runner.py

---
+++ pepper_runner.py
@@ -0,0 +1,32 @@
+import qi # pip install qi is needed to import this library
+import sys 
+
+class Authenticator:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def initialAuthData(self):
+        return {'user': self.username, 'token': self.password}
+
+class AuthenticatorFactory:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def newAuthenticator(self):
+        return Authenticator(self.username, self.password)
+
+# Replace the URL with the IP of Pepper, get the ip from pressing the power button once
+app = qi.Application(sys.argv, url="tcps://10.0.0.4:9503")
+logins = ("nao", "nao")
+factory = AuthenticatorFactory(*logins)
+app.session.setClientAuthenticatorFactory(factory)
+app.start()
+print("started")
+
+
+tts = app.session.service("ALTextToSpeech")
+tts.say("Hello there!")
