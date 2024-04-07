b2291b405145afe06f889d3e7ac9ebef0f9d1ba8
ZachSyv <zachary@syvenky.com>
2024-04-07 13:29:02 -0700

    added function to get images

--- pepper_runner.py
+++ pepper_runner.py
@@ -1,5 +1,7 @@
 import qi # pip install qi is needed to import this library
-import sys 
+import sys
+import time
+from PIL import Image
 
 class Authenticator:
 
@@ -29,10 +31,42 @@ print("started")
 
 #send camera information to local machine
 
+video_service = app.session.service("ALVideoDevice")
+resolution = 2    # VGA
+colorSpace = 11   # RGB
+
+videoClient = video_service.subscribeCamera("python_client", 0, resolution, colorSpace, 5)
+
+t0 = time.time()
+
+# Get a camera image.
+# image[6] contains the image data passed as an array of ASCII chars.
+naoImage = video_service.getImageRemote(videoClient)
+
+t1 = time.time()
+
+# Time the image transfer.
+print("acquisition delay ", t1 - t0)
+
+video_service.unsubscribe(videoClient)
+
+imageWidth = naoImage[0]
+imageHeight = naoImage[1]
+array = naoImage[6]
+image_bytes = bytes(array)
+
+# Create a PIL Image from our pixel array.
+im = Image.frombytes("RGB", (imageWidth, imageHeight), image_bytes)
+
+# Save the image.
+im.save("camImage.png", "PNG")
+
+im.show()
+
 #recive the behaviour detected from the local machine
 
-behaviour_detected = "nailbiting"
+# behaviour_detected = "nailbiting"
 
-if (behaviour_detected == "nailbiting"):
-    tts = app.session.service("ALTextToSpeech")
-    tts.say("Please stop biting your nails")
+# if (behaviour_detected == "nailbiting"):
+#     tts = app.session.service("ALTextToSpeech")
+#     tts.say("Please stop biting your nails")
