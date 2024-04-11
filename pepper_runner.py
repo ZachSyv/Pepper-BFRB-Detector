bd71afdb54c1dfd335385240a5eecb7ad56ee5f4
samuel-antunes <samuel_antunes1@hotmail.com>
2024-04-10 19:10:49 -0700

    implemented changes to make accuracy better and less false positives

--- pepper_runner.py
+++ pepper_runner.py
@@ -35,7 +35,7 @@ logins = ("nao", "nao")
 factory = AuthenticatorFactory(*logins)
 app.session.setClientAuthenticatorFactory(factory)
 app.start()
-print("started")
+# print("started")
 
 #send camera information to local machine
 
@@ -52,61 +52,89 @@ start_time = time.time()
 duration = 60  # seconds
 end_time = time.time()
 
+predictions_dict = {
+    'beard pulling': 0,
+    'hair pulling': 0,
+    'nail bitting': 0,
+    'non-bfrb': 0
+}
+
 print('started')
 
-while end_time - start_time < duration:
+tts = app.session.service("ALTextToSpeech")
+tts.say("Dedicas lbaba o mama o nizar lhbil, tahiya likom mn canada sfu")
 
-    # Get a camera image.
-    # image[6] contains the image data passed as an array of ASCII chars.
-    naoImage = video_service.getImageRemote(videoClient)
-    # print(time.time()-end_time)
+option = 'y'
 
-    if naoImage is not None:
+while option == 'y':
 
-        imageWidth = naoImage[0]
-        imageHeight = naoImage[1]
-        array = naoImage[6]
-        image_bytes = bytes(bytearray(array))
+    option = input('Would you like for pepper to perform a prediction? (y/n) ')
 
-        frame = np.frombuffer(image_bytes, dtype=np.uint8).reshape(imageHeight, imageWidth, 3)
+    while True:
 
-        im = Image.fromarray(frame)
-        im = im.resize((299, 299))
-        im_array = np.array(im)
-        im_array = im_array.astype('float32')/255.0
-        im_array = np.expand_dims(im_array, axis = 0)
-        prediction = model.predict(im_array)
-        predicted_classes = np.argmax(prediction, axis=1)
+        # Get a camera image.
+        # image[6] contains the image data passed as an array of ASCII chars.
+        naoImage = video_service.getImageRemote(videoClient)
+        # print(time.time()-end_time)
 
-        if predicted_classes[0] != 3 and np.max(prediction) > 0.6:
-            behavior = classes[predicted_classes[0]]
-            break
+        if naoImage is not None:
+
+            imageWidth = naoImage[0]
+            imageHeight = naoImage[1]
+            array = naoImage[6]
+            image_bytes = bytes(bytearray(array))
+
+            frame = np.frombuffer(image_bytes, dtype=np.uint8).reshape(imageHeight, imageWidth, 3)
+
+            im = Image.fromarray(frame)
+            im = im.resize((299, 299))
+            im_array = np.array(im)
+            im_array = im_array.astype('float32')/255.0
+            im_array = np.expand_dims(im_array, axis = 0)
+            prediction = model.predict(im_array)
+            predicted_classes = np.argmax(prediction, axis=1)
+
+            if predicted_classes[0] != 3 and np.max(prediction) > 0.6:
+                behavior = classes[predicted_classes[0]]
+                tts = app.session.service("ALTextToSpeech")
+
+                print(prediction)
+
+                predictions_dict[behavior] += 1
+
+                if predictions_dict[behavior] > 2:
+                    if behavior == 'hair pulling':
+                        tts.say("You're your hair")
+                    elif behavior == 'beard pulling':
+                        tts.say("You're pulling your beard")
+                    elif behavior == 'nail bitting':
+                        tts.say("You're biting your nails")
+                    else:
+                        tts.say("you're fine")
+                    predictions_dict = {
+                        'beard pulling': 0,
+                        'hair pulling': 0,
+                        'nail bitting': 0,
+                        'non-bfrb': 0
+                    }
+                    
+                    break
+            else:
+                print(classes[predicted_classes[0]])
+                behavior = 'non-bfrb'
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)    
+        
+            out.write(frame)
+            end_time = time.time()
+            print("Current delay", end_time - start_time)
         else:
-            print(classes[predicted_classes[0]])
+            print('image not received')
             behavior = 'non-bfrb'
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)    
-    
-        out.write(frame)
-        end_time = time.time()
-        print("Current delay", end_time - start_time)
-    else:
-        print('image not received')
-        behavior = 'non-bfrb'
-    
+        
 
 video_service.unsubscribe(videoClient)
 out.release()
 
 print('finished recording')
 
-tts = app.session.service("ALTextToSpeech")
-
-if behavior == 'hair pulling':
-    tts.say("Stop pulling your hair")
-elif behavior == 'beard pulling':
-    tts.say("Stop pulling your beard")
-elif behavior == 'nail bitting':
-    tts.say("Stop biting your nails")
-else:
-    tts.say("you're fine")
